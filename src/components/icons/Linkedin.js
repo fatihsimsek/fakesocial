21835d941244a87f2c9a58afae4b7cabfc2d6dc7
Fatih Simsek <fatih.simsek.cm@gmail.com>
2024-12-04 09:23:10 +0300

    update settings

---
+++ src/components/icons/Linkedin.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Svg, {Path, Circle} from 'react-native-svg';
+const SvgLinkedin = props => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    className=""
+    viewBox="0 0 24 24"
+    {...props}>
+    <Path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6M2 9h4v12H2z" />
+    <Circle cx={4} cy={4} r={2} />
+  </Svg>
+);
+export default SvgLinkedin;
--- src/views/SettingsView.js
+++ src/views/SettingsView.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { GithubIcon, LinkedinIcon } from '../components/icons';
 
 function SettingsView() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Settings</Text>
+      <View style={{marginTop:4}}>
+        <View style={{margin:2, paddingVertical:5, backgroundColor:'#E5E5E5'}}>
+          <View>
+            <Text style={{fontWeight:'600', fontSize:18, padding:10}}>Fatih Şimşek</Text>
+            <View style={{flexDirection:'row', alignContent:'center', padding:5 }}>
+                <LinkedinIcon size={28} color="black"></LinkedinIcon>
+                <Text style={{marginLeft:5, alignSelf:'center', fontWeight:'500'}}>https://www.linkedin.com/in/fatih-simsek-dev</Text>
+            </View>
+            <View style={{flexDirection:'row', alignContent:'center', padding:5 }}>
+                <GithubIcon size={28} color="black"></GithubIcon>
+                <Text style={{marginLeft:5, alignSelf:'center', fontWeight:'500'}}>https://www.github.com/fatihsimsek</Text>
+            </View>
+          </View>
+        </View>
       </View>
     );
   }
