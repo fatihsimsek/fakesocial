272db7b0e8fdb7450d59a7a9ae44e941dca5b8d1
Fatih Simsek <fatih.simsek.cm@gmail.com>
2024-12-16 00:02:42 +0300

    fix image

--- src/components/instagram/InstagramMessageBubble.js
+++ src/components/instagram/InstagramMessageBubble.js
@@ -5,24 +5,31 @@ import { CheckIcon } from '../icons';
 
 function InstagramMessageBubble({data}) {
     const isMyMessage = data.messageType === ConversationMessageType.SEND;
-    return (
+    if(data.imageUrl?.length > 0){
+      return (
         <View style={{
-                ...styles.messageContainer,
-                alignSelf: isMyMessage ? "flex-end": "flex-start",
-                backgroundColor: isMyMessage ? "#f5f5f5" : "#ffffff",
-                borderBottomLeftRadius: 10,
-                borderBottomRightRadius: 10}}>
+          ...styles.imageContainer,
+          alignSelf: isMyMessage ? "flex-end": "flex-start"}}>
+          <Image source={{uri: data.imageUrl}} style={{width:"98%", paddingTop:10, paddingLeft:10, height: 150}}></Image>
+        </View>
+      );
+    }
+    else {
+      return (
+        <View style={{
+          ...styles.messageContainer,
+          alignSelf: isMyMessage ? "flex-end": "flex-start",
+          backgroundColor: isMyMessage ? "#f5f5f5" : "#ffffff",
+          borderBottomLeftRadius: 10,
+          borderBottomRightRadius: 10}}>
             <View style={{flexDirection:'column'}}>
-                {
-                  (data.imageUrl?.length > 0) && 
-                  <Image source={{uri: data.imageUrl}} style={{width:"98%", paddingTop:10, paddingLeft:10, height: 150}}></Image>
-                }
                 <View style={{flexDirection:'row'}}>
                   <Text style={styles.messageText}>{data.content}</Text>
                 </View>
             </View>
         </View>
-    );
+      );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -35,6 +42,13 @@ const styles = StyleSheet.create({
       borderRadius: 10,
       borderColor: '#e9e9e9',
       borderWidth:1
+    },
+    imageContainer: {
+      width: "66%",
+      marginVertical: 3,
+      marginHorizontal: 10,
+      paddingVertical: 5,
+      flexDirection: "row"
     },
     messageText: {
       fontSize: 14,
